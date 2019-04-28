5fd0b206961505d7bcf5fe874ec0524b74d130b0
Metatronxl <8b:b6:7d:29:b0:c3:da:1d:d3:3d:58:6f:d7:76:32:9e>
2019-04-28 17:27:25 +0800

    Initial commit

---
+++ src/main/java/com/noisy/proxyprob/ProxyResController.java
@@ -0,0 +1,31 @@
+package com.noisy.proxyprob;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author lei.X
+ * @date 2019/4/26
+ */
+
+@Slf4j
+@RestController
+@RequestMapping("/test")
+public class ProxyResController {
+
+    @ResponseBody
+    @GetMapping("/parse")
+    public String parseRequest(HttpServletRequest request){
+
+        String ipAddress = IpUtil.getIpAddr(request);
+
+        return  ipAddress;
+
+    }
+}
---
+++ src/main/java/com/noisy/proxyprob/ProxyprobApplication.java
@@ -0,0 +1,13 @@
+package com.noisy.proxyprob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProxyprobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProxyprobApplication.class, args);
+	}
+
+}
